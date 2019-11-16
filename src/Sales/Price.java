021fa9bbf00da47a15536cb4479e4e18cd99904c
Sean <11585008+Techatrekka@users.noreply.github.com>
2019-11-16 22:13:59 +0000

    Update Price.java

--- src/Sales/Price.java
+++ src/Sales/Price.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 
 public class Price {
 
-    private double[][] OldPrices;
+    private double[][] Prices ;
 
-    public double[][] setWeeklyPrices(Room room, double[] NewPrices, double[][] OldPrices, ArrayList<Room> RoomTypes) {
+    public double[][] setWeeklyPrices(Room room, double[] NewPrices,
+                                      double[][] OldPrices, ArrayList<Room> RoomTypes) {
         int row = 0;
         for (int i = 0; i < RoomTypes.size(); i++) {
             if (room.toString().equals(RoomTypes.get(i).toString())) {
@@ -17,6 +18,11 @@ public class Price {
         for (int i = 0; i < NewPrices.length; i++) {
             OldPrices[row][i] = NewPrices[i];
         }
+        this.Prices = OldPrices;
         return OldPrices;
+    }
+
+    public double[][] getPrices() {
+        return Prices;
     }
 }
