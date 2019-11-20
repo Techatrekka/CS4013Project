3a09dccdecc9fecbf454df170a106d9c20485e3a
Sean <11585008+Techatrekka@users.noreply.github.com>
2019-11-20 16:12:11 +0000

    update

--- src/L4/L4.java
+++ src/L4/L4.java
@@ -4,19 +4,14 @@ import java.util.ArrayList;
 
 public class L4 {
     ArrayList<Hotel> L4 = new ArrayList<>();
-    int rating;
 
-    void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    boolean addHotel(String name, String location, int rating, String[] roomDetails, int[] numOfRooms) {
+    public boolean addHotel(String name, String location, int rating, String[] roomDetails, int[] numOfRooms) {
         Hotel h = new Hotel(name, location, rating, roomDetails, numOfRooms);
         L4.add(h);
         return true;
     }
 
-    boolean removeHotel(int index) {
+    public boolean removeHotel(int index) {
         L4.remove(index);
         return true;
     }
--- src/Menu.java
+++ src/Menu.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import L4.Hotel;
+import L4.L4;
 import L4.Room;
 import People.Customer;
 import People.Supervisor;
@@ -17,6 +18,18 @@ public class Menu {
     Hotel hotel = new Hotel();
     ArrayList<Room> rooms = new ArrayList<>();
     int numOfNights;
+
+    public void initialise() {
+        L4 chain = new L4();
+        chain.addHotel("MiggysParadise","Mamma Mia Land", 5,
+                new String[]{"Bachelor suite, 1, true", "Two to tango, 2, true",
+                        "bring the family suite, 5, true"},
+                new int[]{2,3,3});
+        chain.addHotel("SeanVille","HoesAin'tLoyalCentral",4,
+                new String[]{"I went to the gym once Suite, 1, true"}, new int[]{300});
+        chain.addHotel("WickyNicky'sEmporium","LoserVille", -1,
+                new String[]{"Slow Head Turn Deluxe, 1, false"}, new int[] {1});
+    }
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
@@ -38,12 +51,12 @@ public class Menu {
                 Object[] options = new Object[]{"Reservation", "Cancellation"};
                 Object option = getOptions(options);
                 assert option != null;
-                if (option.toString().toUpperCase().equals("A")||option.equals( 'A'))
+                if (option.equals("Reservation"))
                 {
                     boolean done = false;
                     while(!done) {
                         System.out.println("What room would you like");
-                        option = getOptions(hotel.getRoomTypes().toArray()).toString();
+                        option = getOptions(hotel.getRoomTypes().toArray());
                         for (int i = 0; i < hotel.getRoomTypes().size(); i++) {
                             if (option.equals(hotel.getRoomTypes().toString())) {
                                 rooms.add(hotel.getRoomTypes().get(i));
@@ -146,9 +159,9 @@ public class Menu {
                                 info[3] = scanner.next();
                                 Customer customer = new Customer(info[0], info[1], info[2], info[3]);
                                 System.out.println("Would you like to make a Reservation or Cancellation");
-                                options = new Object[]{"Reservation", "B) Cancellation"};
+                                options = new Object[]{"Reservation", "Cancellation"};
                                 option = getOptions(options);
-                                if (option.toString().equals("A"))
+                                if (option.toString().equals("Reservation"))
                                 {
                                     boolean done = false;
                                     while(!done) {
@@ -172,7 +185,7 @@ public class Menu {
                                     customer.makeReservation(checkIn, numOfNights, rooms, true, hotel.getRoomTypes());
                                 }
                                 break;
-                            case "B":
+                            case "Cancellation":
                                 System.out.println("Enter Reservation Number: ");
                                 option = scanner.next();
                                 if ((readReservation(option.toString(), "Reservations.csv") != null)) {
@@ -202,15 +215,13 @@ public class Menu {
 		{
 			return null;
 		}
-        int i;
-			char c = 'A';
-			for (Object option : options)
-			{
-				System.out.println(c + ") " + option); // Choice requires toString method 
-				c++;
-			}
-			String input = scanner.nextLine().toUpperCase();
-			i = input.toUpperCase().charAt(0) - 'A';
-		return options[i];
+        char c = 'A';
+        for (int i = 0; i < options.length; i++) {
+            System.out.println(c + ") " + options[i]);
+            c++;
+        }
+		char x = scanner.next().toUpperCase().charAt(0);
+        int h = x - 65;
+		return options[h];
 	}
 }
