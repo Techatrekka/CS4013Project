61e9d06fae6c30856fe32f6f2ca4217dcb6cf6a4
nicole-berty <47594782+nicole-berty@users.noreply.github.com>
2019-11-25 15:23:17 +0000

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -40,32 +40,37 @@ public class Menu {
         initialise();
         boolean run = true;
         while (run) {
+            String[] nickyboo = new String[]{"Yes", "No"};
             hotelChosen = getOptions(hotels).toString();
-            System.out.println("C)ustomer S)taff Q)uit");
+            System.out.println("A) Customer B) Staff C) Quit");
             String choice = scanner.nextLine();
             choice = choice.toUpperCase();
-            if (choice.equals("C")) {
-                System.out.println("Please input Name, email, phone, address");
-                String[] info = new String[4];
-                info[0] = scanner.nextLine();
-                info[1] = scanner.nextLine();
-                info[2] = scanner.nextLine();
-                info[3] = scanner.nextLine();
-                Customer customer = new Customer(info[0], info[1], info[2], info[3]);
-                System.out.println("Would you like to make a Reservation or Cancellation");
+            if (choice.equals("A")) {
+                System.out.println("Please input: Name, Email, Phone, Address");
+                Scanner sc = new Scanner(System.in);
+                String info = sc.nextLine();
+                String[] split = info.split(",");
+                Customer customer = new Customer(split[0], split[1], split[2], split[3]);
+                System.out.println("Would you like to make a Reservation or a Cancellation?");
                 Object[] options = new Object[]{"Reservation", "Cancellation"};
                 Object option = getOptions(options);
                 assert option != null;
                 if (option.equals("Reservation")) {
                     makeReservation(customer);
+                    System.out.println("Would you like to make another reservation or cancellation? Yes/No");
+                    option = null;
+                    option = getOptions(nickyboo);
+                    if (option.equals("No")) {
+                        run = false;
+                    }
                 } else {
                     makeCancellation();
                 }
-            } else if (choice.equals("S")) {
+            } else if (choice.equals("B")) {
                 Object[] options = {"Supervisor", "Administrator"};
                 Object option = getOptions(options);
                 switch (option.toString()) {
-                    case "A":
+                    case "Supervisor":
                         options = new Object[]{"Reservation", "Cancellation", "giveDiscount", "RequestAnalysis"};
                         option = getOptions(options);
                         switch (option.toString()) {
@@ -77,9 +82,21 @@ public class Menu {
                                 info[3] = scanner.nextLine();
                                 Customer customer = new Customer(info[0], info[1], info[2], info[3]);
                                 makeReservation(customer);
+                                System.out.println("Would you like to make another reservation or cancellation? Yes/No");
+                                option = null;
+                                option = getOptions(nickyboo);
+                                if (option.equals("No")) {
+                                    run = false;
+                                }
                                 break;
                             case "Cancellation":
                                 makeCancellation();
+                                System.out.println("Would you like to make another reservation or cancellation? Yes/No");
+                                option = null;
+                                option = getOptions(nickyboo);
+                                if (option.equals("No")) {
+                                    run = false;
+                                }
                                 break;
                             case "giveDiscount":
                                 Supervisor supervisor = new Supervisor();
@@ -92,7 +109,7 @@ public class Menu {
                                 break;
                         }
                         break;
-                    case "B":
+                    case "Administrator":
                         options = new Object[]{"Reservation", "Cancellation"};
                         option = getOptions(options);
                         switch (option.toString()) {
@@ -104,14 +121,26 @@ public class Menu {
                                 info[3] = scanner.nextLine();
                                 Customer customer = new Customer(info[0], info[1], info[2], info[3]);
                                 makeReservation(customer);
+                                System.out.println("Would you like to make another reservation or cancellation? Yes/No");
+                                option = null;
+                                option = getOptions(nickyboo);
+                                if (option.equals("No")) {
+                                    run = false;
+                                }
                                 break;
                             case "Cancellation":
                                 makeCancellation();
+                                System.out.println("Would you like to make another reservation or cancellation? Yes/No");
+                                option = null;
+                                option = getOptions(nickyboo);
+                                if (option.equals("No")) {
+                                    run = false;
+                                }
                                 break;
                         }
                         break;
                 }
-            } else if (choice.equals("Q")) {
+            } else if (choice.equals("C")) {
                 run = false;
             } else {
                 System.out.println("Choice not recognised, choose again!");
@@ -139,7 +168,7 @@ public class Menu {
         String choice = "";
         Object option = null;
         while (!done) {
-            System.out.println("What room would you like");
+            System.out.println("What type of room would you like?");
             int hotelInArray = 0;
             for (int i = 0; i < chain.getL4().size(); i++) {
                 if (chain.getL4().get(i).getName().equals(hotelChosen)) {
@@ -153,16 +182,16 @@ public class Menu {
                     rooms.add(chain.getL4().get(hotelInArray).getRoomTypes().get(i));
                 }
             }
-            System.out.println("Would you like to add another room? Y/N");
+            System.out.println("Would you like to add another room? Yes/No");
             choice = scanner.nextLine().toUpperCase();
-            if (!choice.equals("Y")) {
+            if (!choice.equals("Yes")) {
                 done = true;
             }
         }
         System.out.println("When would you check in?(dd-mm-yyyy)");
         String dateFormat   = "dd-MM-yyyy";
         LocalDate checkIn = LocalDate.parse(scanner.nextLine(),DateTimeFormatter.ofPattern(dateFormat));
-        System.out.println("How many nights are you saying?");
+        System.out.println("How many nights are you staying?");
         numOfNights = scanner.nextInt();
         customer.makeReservation(checkIn, numOfNights, rooms, true, hotel.getRoomTypes(), hotelChosen);
     }
@@ -174,7 +203,7 @@ public class Menu {
         if ((readReservation(option.toString(), hotelChosen + "Reservations.csv") != null)) {
             Reservations re = readReservation(option.toString(), hotelChosen + "Reservations.csv");
             reservations.deleteReservations(re);
-            System.out.println("Deleted");
+            System.out.println("Reservation has been deleted.");
         }
     }
 }
