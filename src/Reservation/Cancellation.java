be2168264abad607bc771574263752fe1b945509
mighelecava <44195983+mighelecava@users.noreply.github.com>
2019-11-27 14:39:05 +0000

    Update Cancellation.java

--- src/Reservation/Cancellation.java
+++ src/Reservation/Cancellation.java
@@ -9,19 +9,18 @@ public class Cancellation extends Reservation
 {
     private LocalDate cancellationDate;
 
-    public Cancellation(String reservationId, String reservationName, String number, String email, LocalDate checkIn, LocalDate cancellationDate, ArrayList<Room> rooms, double totalCost, boolean advancedPurchase)
+    public Cancellation(String reservationId, String reservationName, String number, String email, LocalDate checkIn,
+                        LocalDate cancellationDate, ArrayList<Room> rooms, double totalCost, boolean advancedPurchase)
     {
         super(reservationId, reservationName, number, email, checkIn, 0, rooms, totalCost, advancedPurchase);
         this.cancellationDate = cancellationDate;
     }
 
-    public Cancellation(LocalDate cancellationDate, Reservation reservations,String hotelChosen)
+    public Cancellation(Reservation reservations,String hotelChosen)
     {
-        this.cancellationDate = cancellationDate;
-        deleteReservation(reservations,hotelChosen+"Cancellations.csv");
-        ArrayList<Reservation> cancels = readFromCSV("Cancellations.csv");
+        ArrayList<Reservation> cancels = readFromCSV(hotelChosen+"Cancellations.csv");
         cancels.add(reservations);
-        writeToCSV("Cancellations.csv",cancels,true);
+        writeToCSV(hotelChosen+"Cancellations.csv",cancels,true);
     }
 
     public LocalDate getCancellationDate()
