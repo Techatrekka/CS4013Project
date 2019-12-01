b9ec0a32ab501b3573f2f610c4d61e557d809632
nicole-berty <47594782+nicole-berty@users.noreply.github.com>
2019-12-01 00:28:13 +0000

    Update Room.java

--- src/L4/Room.java
+++ src/L4/Room.java
@@ -1,5 +1,9 @@
 package L4;
 
+/**
+ * @author:
+ */
+
 public class Room extends Hotel {
 
 	public String type;
@@ -8,32 +12,60 @@ public class Room extends Hotel {
 
 	Room(){}
 
+	/**
+	 * Creates a room object
+	 * @param type
+	 * @param occupancy
+	 * @param breakfastIncluded
+	 */
    public Room (String type, String occupancy, boolean breakfastIncluded) {
 		this.type = type;
 		this.occupancy = occupancy;
 		this.breakfastIncluded = breakfastIncluded;
 	}
 
+	/**
+	 * Sets the type of room
+	 * @param type
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	/**
+	 * @return the room type
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * Sets the occupancy of a room
+	 * @param occupancy
+	 */
 	void setOccupancy(String occupancy) {
 		this.occupancy = occupancy;
 	}
 
+	/**
+	 * Gets the occupancy of a room
+	 * @return
+	 */
 	public String getOccupancy() {
 		return occupancy;
 	}
 
+	/**
+	 * Set whether breakfast is included in a room.
+	 * @param breakfastIncluded
+	 */
 	void setBreakfastIncluded(boolean breakfastIncluded) {
 		this.breakfastIncluded = breakfastIncluded;
 	}
 
+	/**
+	 * @return true or false based on whether breakfast is included with a room
+	 */
 	public boolean isBreakfastIncluded() {
 		return breakfastIncluded;
 	}
@@ -46,6 +78,9 @@ public class Room extends Hotel {
 		return occupancy;
 	}
 
+	/**
+	 * @return room details formatted for CSV file
+	 */
 	public String toCSV()
 	{
 		return getType() + "_" + getOccupancy() + "_" + isBreakfastIncluded();
