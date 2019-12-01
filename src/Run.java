d59efc62b7cbdf3089294c9776e1da3d9270aef8
nicole-berty <47594782+nicole-berty@users.noreply.github.com>
2019-12-01 00:33:14 +0000

    Update Run.java

--- src/Run.java
+++ src/Run.java
@@ -1,5 +1,14 @@
+/**
+ * @author:
+ */
+
+
 public class Run {
 
+    /**
+     * Run the program
+     * @param args
+     */
     public static void main(String[]   args) {
         Menu menu = new Menu();
         menu.run();
