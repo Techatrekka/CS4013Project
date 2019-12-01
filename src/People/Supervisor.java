d2c6c24db7cbb58bbf5f6375d1a7fcec00cabe7d
nicole-berty <47594782+nicole-berty@users.noreply.github.com>
2019-12-01 00:31:27 +0000

    Update Supervisor.java

--- src/People/Supervisor.java
+++ src/People/Supervisor.java
@@ -4,11 +4,21 @@ import Reservation.*;
 
 import java.util.ArrayList;
 
+/**
+ * @author:
+ */
+
 public class Supervisor extends Staff {
 
     public Supervisor(){}
 
-
+    /**
+     * Create a supervisor object
+     * @param name
+     * @param email
+     * @param phone
+     * @param wages
+     */
     Supervisor(String name, String email, String phone, double wages) {
         super.name = name;
         super.email = email;
@@ -16,14 +26,28 @@ public class Supervisor extends Staff {
         super.wages = wages;
     }
 
+    /**
+     * Set the wages of the supervisor
+     * @param wages
+     */
     private void setWages(double wages) {
         this.wages = wages;
     }
 
+    /**
+     * Get the wages of the supervisor
+     * @return wages
+     */
     private double getWages() {
         return this.wages;
     }
 
+    /**
+     * Give a discount on a specified reservation in a specified hotel
+     * @param reservation
+     * @param discount
+     * @param hotel
+     */
     public void giveDiscount(Reservation reservation, double discount,String hotel) {
         double cost = reservation.getTotalCost();
         double newPrice = cost * (1 - (discount/ 100));
@@ -34,9 +58,6 @@ public class Supervisor extends Staff {
             }
         }
         Reservation.writeToCSV(hotel + "Reservations.csv",r1,true);
-    }
-
-    void requestDataAnalysis() {
     }
 
     @Override
